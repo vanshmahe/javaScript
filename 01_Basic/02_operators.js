2b683409bd704494cb27406386c5f87a8cb2b84d
vanshmahe <145955801+vanshmahe@users.noreply.github.com>
2023-09-27 05:40:37 +0000

    Arithmetic assignment logical operators done

---
+++ 01_Basic/02_operators.js
@@ -0,0 +1,72 @@
+// Arithmetic
+
+let x=10,y=2;
+// console.log(`${x} + ${y} = ${x + y}`);
+// console.log(`${x} - ${y} = ${x - y}`);
+// console.log(`${x} * ${y} = ${x * y}`);
+// console.log(`${x} / ${y} = ${x / y}`);
+// console.log(`${x} % ${y} = ${x % y}`);
+// console.log(`${x} ** ${y} = ${x ** y}`);
+// ++x;
+// console.log(`++ ${x} = ${x}`);
+// x++;
+// console.log(` ${x}++ = ${x}`);
+
+// //--x;
+// console.log(`--${x} = ${--x}`);
+// //x--;
+// console.log(` ${x}-- = ${x--}`);
+
+
+
+// x=5;
+// y=++x;
+
+// console.log(`x = ${x} and y= ${y}`);
+
+// x=5;
+// y=x++;
+
+// console.log(`x = ${x} and y= ${y}`);
+
+// x=5;
+// y=--x;
+// console.log(`x = ${x} and y=${y}`);
+// x=5; 
+// y=x--;
+// console.log(`x=${x} and ${y}`); 
+
+// shorthand assignment
+
+// x=5;y=10;
+
+// x+=y;   // x= x+ y;
+// console.log(x);
+
+// Comparison Operators
+
+// x=5; y=5;
+// let z="5";
+
+// console.log(` x = ${x}   y= ${y}    z= ${z}`);
+
+// console.log(`x == y    ${x==y}`);
+// console.log(`x == z    ${x==z}`);
+// console.log(`x === z    ${x===z}`);
+
+// Logical Operators 
+/*
+Operator	Description
+&&	logical and
+||	logical or
+!	logical not
+*/
+
+// x=0; y=5;
+// let z=7;
+
+// console.log(` x==y && x==z ${ (x==y) && (x==z)}`);
+// console.log(` x==y || x==z ${ (x==y) || (x==z)}`);
+// console.log(` x= ${x}  !x=${!x}`);
+
+
