a8c72e4a2785601bc6b0c8c56814b4c95980e906
vanshmahe <145955801+vanshmahe@users.noreply.github.com>
2023-10-11 04:42:24 +0000

    String search complete

---
+++ 01_Basic/06_stringSearch.js
@@ -0,0 +1,25 @@
+//let text = "Please locate where 'locate' occurs!";
+//console.log(text.indexOf("locate"));
+//console.log(text.lastIndexOf("locate"));
+//console.log(text.lastIndexOf("locate",10));
+
+// console.log(text.search("locate"));
+// console.log(text.search(/locate/));
+
+let text = "The rain in SPAIN stays mainly in the plain";
+//console.log(text.match(/ain/));
+//console.log(text.match("ain"));
+//console.log(text.match(/ain/g));
+//console.log(text.match(/ain/gi));
+
+// let iterator=text.matchAll(/ain/g);
+// console.log(Array.from(iterator));
+
+//console.log(text.includes("rain"));
+//console.log(text.includes("rain",5));
+
+// console.log(text.startsWith("hello"));
+// console.log(text.startsWith("the",5));
+
+// console.log(text.endsWith("plain",0));
+// console.log(text.endsWith("plain"));
