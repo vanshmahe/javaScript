23ca34a2c8132d1eeb9ae107d4c794ddd4539b37
vanshmahe <145955801+vanshmahe@users.noreply.github.com>
2023-10-12 05:20:08 +0000

    string templates

---
+++ 01_Basic/07_stringTemplate.js
@@ -0,0 +1,11 @@
+//let text = `He's often called "Johnny"`;
+
+// let text =
+// `The quick
+// brown fox
+// jumps over
+// the lazy dog`;
+//console.log(text);
+
+let a=10,b=20;
+console.log(`${a} + ${b} = ${a+b}`);
