e95fad38f39c6786fa4a91f0a117d965242bd021
Vansh Mahe <vanshmahe2004@gmail.com>
2023-10-18 10:15:08 +0530

    Array

---
+++ 02_Basic/01_Arrays.js
@@ -0,0 +1,27 @@
+const cars=[
+    "alto",
+    "k10",
+    "swift",
+    "mustang"
+];
+// const cars= [];
+// cars[0]= "Saab";
+// cars[1]= "Volvo";
+// cars[2]= "BMW";
+//let car=cars[0];
+
+//array to string
+
+//let car=cars.toString();
+
+
+//let lastCar=cars[cars.length-1]
+
+// add new element
+// cars.push("i10");
+// cars[cars.length]="i20";
+
+
+//let result=Array.isArray(cars);
+let result= cars instanceof Array;
+document.getElementById("demo").innerHTML=result;
