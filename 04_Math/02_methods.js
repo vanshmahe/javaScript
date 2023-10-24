f9c6d70684a0a2dfbd9734f940e90d216b47e024
vanshmahe <145955801+vanshmahe@users.noreply.github.com>
2023-10-24 05:51:22 +0000

    math methods

---
+++ 04_Math/02_methods.js
@@ -0,0 +1,36 @@
+let x=12.9,y=12.1;
+let a=-12.9,b=-12.1;
+// console.log(`Rounded ${x}  is  ${Math.round(x)}`);
+// console.log(`Rounded ${y}  is  ${Math.round(y)}`);
+// console.log(`Ceil ${x}  is  ${Math.ceil(x)}`);
+// console.log(`Ceil ${y}  is  ${Math.ceil(y)}`);
+//console.log(`Floor ${a}  is  ${Math.floor(a)}`);
+//console.log(`Floor ${b}  is  ${Math.floor(b)}`);
+//console.log(`Ceil ${a} is ${Math.ceil(a)}`);
+//console.log(`Ceil ${b} is ${Math.ceil(b)}`);
+//console.log(`Rounded ${a}  is  ${Math.round(a)}`);
+//console.log(`Rounded ${b}  is  ${Math.round(b)}`);
+// console.log(`Trunc ${a}  is  ${Math.trunc(a)}`);
+// console.log(`Trunc ${b}  is  ${Math.trunc(b)}`);
+
+// let s=16;
+// console.log(`Pow(2,3)  is  ${Math.pow(2,3)}`);
+// console.log(`Sqrt of  ${s}  is  ${Math.sqrt(s)}`);
+
+// console.log(` Absolute value of ${a} is ${Math.abs(a)}`);
+
+// let angle=45;
+// console.log(` Sin value of ${angle} is ${Math.sin(angle)}`);
+// console.log(` Cos value of ${angle} is ${Math.cos(angle)}`);
+
+// const myArray=[1,2,3,4,5,6,7,8,9,10];
+
+// console.log(`Minimum value is ${Math.min(1,2,3,4,5,6,7)}`);
+// console.log(`Maximum value is ${Math.max(3,2,4,1,2,3,.4)}`);
+
+//console.log(Math.floor(Math.random()*100));
+
+// for (let index = 0; index < 10; index++) {
+//     console.log(Math.floor(Math.random()*100));
+    
+// }
