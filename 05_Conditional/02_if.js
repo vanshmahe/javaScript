494eb681aba49fecebb6ff648f4c6fc6c1a8591a
vanshmahe <145955801+vanshmahe@users.noreply.github.com>
2023-10-25 05:47:31 +0000

    if

---
+++ 05_Conditional/02_if.js
@@ -0,0 +1,48 @@
+// if
+
+const age=19;
+let age2=10;
+
+// if(age >=18){
+//     console.log("you can vote");
+// }
+
+// if-else
+
+// if(age2 >=18){
+//     console.log("you can vote");
+// } else{
+//     console.log("you cannot vote");
+// }
+
+// let dbp=85,ubp=120;
+
+// if(dbp === 80 && ubp===120){
+//     console.log("your BP is normal");
+// }else{
+//     console.log("your BP is not normal");
+// }
+
+
+// else-if
+
+// let day=8;
+
+// if(day === 1){
+//     console.log("Monday");
+// } else if(day === 2){
+//     console.log("Tuesday");
+// }else if(day === 3){
+//     console.log("Wednesday");
+// }else if(day === 4){
+//     console.log("Thursday");
+// } else if(day === 5){
+//     console.log("Friday");
+// }else if(day === 6){
+//     console.log("Saturday");
+// }else if(day === 7){
+//     console.log("Sunday");
+// }else{
+//     console.log("Enter valid day Number");
+// }
+
