05f2e5e841c78f3c46fc9c203c1fbcbec876e554
vanshmahe <145955801+vanshmahe@users.noreply.github.com>
2023-10-25 04:58:26 +0000

    boolean

---
+++ 04_Math/04_boolean.js
@@ -0,0 +1,22 @@
+//let x = 0;
+//let x = -0;
+//let x = 1;
+// let x = "";
+// let x;
+// let x=null;
+// let x=false;
+// let x=NaN;
+
+
+// let x=true;
+// let y= new Boolean(true);  // not use this
+//console.log("Type of x  ="+ typeof(x) + "   Type of y =  "+ typeof(y));
+
+// let x=new Boolean(true);
+// let y= new Boolean(true);
+// console.log(x===y);
+
+// let x=true;
+// let y=true;
+// console.log(x==y);
+// console.log(x===y);
