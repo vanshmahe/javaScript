ed08bcc11ab76c0f2e03b1d8aa27d4cd2af2c0be
vanshmahe <145955801+vanshmahe@users.noreply.github.com>
2023-11-01 07:22:04 +0000

    while do-while

---
+++ 06_Iteration/02_while_dowhile.js
@@ -0,0 +1,14 @@
+const cars = ["BMW", "Volvo", "Saab", "Ford", "Fiat", "Audi"];
+const length=cars.length;
+// let i=0;
+// while (i<length) {
+//     console.log(cars[i]);
+//     i++;
+// }
+
+// do while runs atleast once even if condition is false
+let i=0;
+do {
+    console.log("hi");
+} while (i<0);
+
