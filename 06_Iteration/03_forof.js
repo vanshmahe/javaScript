1a16bf7a24e5992db08b09a36ee22a1fec741ebb
vanshmahe <145955801+vanshmahe@users.noreply.github.com>
2023-11-01 07:30:03 +0000

    for of

---
+++ 06_Iteration/03_forof.js
@@ -0,0 +1,13 @@
+// const cars = ["BMW", "Volvo", "Mini"];
+
+// for (const car of cars) {
+//     console.log(car);
+// }
+
+const numbers=[10,20,56,65,65,78,45];
+let sum=0;
+for(const number of numbers){
+    sum+=number;
+
+}
+console.log(sum);
