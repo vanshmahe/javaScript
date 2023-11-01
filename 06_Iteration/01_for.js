d3980d5415120d14b5c8e16e72815c39934f46d9
vanshmahe <145955801+vanshmahe@users.noreply.github.com>
2023-11-01 07:15:25 +0000

    for loop

---
+++ 06_Iteration/01_for.js
@@ -0,0 +1,13 @@
+// const cars = ["BMW", "Volvo", "Saab", "Ford", "Fiat", "Audi"];
+// const length=cars.length;
+
+// for(let i=0;i<length;i++){
+//     console.log(cars[i]);
+// }
+let numbers=[12,15,54,45,27,89,98];
+for (let index = 0; index < numbers.length; index++) {
+   
+    if(numbers[index] % 2 == 0){
+        console.log(numbers[index]);
+    }
+}
